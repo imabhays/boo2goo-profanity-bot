47430222c5177e1a2c48b448291140269906bdcd
absecnet <abhays_bca2107007@ahead.students.amrita.edu>
2023-06-08 17:34:16 +0530

    bot upload

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Projects\Learning\python\telegram-bots\googoo\env
+set VIRTUAL_ENV=D:\Projects\Learning\python\telegram-bots\boo2goo-profanity-bot\env
 
 if not defined PROMPT set PROMPT=$P$G
 
